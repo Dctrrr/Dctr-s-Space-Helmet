1c1c0243a8f4031c1ceec74becd7abd2d0b33a13
Dctrrr_ <87957190+Dctrrr@users.noreply.github.com>
2022-05-11 17:34:26 +0530

    Update SpaceHelmetCmd.java

--- src/main/java/space/helm/SpaceHelmetCmd.java
+++ src/main/java/space/helm/SpaceHelmetCmd.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
-import space.helm.dctr;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,33 +11,36 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SpaceHelmetCmd implements CommandExecutor {
 
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-                if (args.length >= 1){
+        if (args.length >= 1) {
+            String playerName = args[0];
+            Player targetPlayer = Bukkit.getPlayer(playerName);
+        }
 
-                        String playerName = args[0];
-                        Player targetPlayer = Bukkit.getPlayer(playerName);
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
 
-                }
+            if (args.length >= 1) {
+                String playerName = args[0];
+                player = Bukkit.getPlayer(playerName);
+            }
 
+            assert player != null;
+            if (player.hasPermission("specialgift.dctrspacehelm")) {
+                ItemStack itemStack = new ItemStack(Material.RED_STAINED_GLASS);
 
-                if (sender instanceof Player) {
-                        Player player = (Player)sender;
-                        if (args.length >= 1) {
-                                String playerName = args[0];
-                                player = Bukkit.getPlayer(playerName);
-                        }
-                        if (player.hasPermission("dctr.givespacehelm"));
-                        ItemStack itemStack = new ItemStack(Material.RED_STAINED_GLASS);
-                        ItemMeta itemMeta = itemStack.getItemMeta();
-                        itemMeta.setDisplayName(ChatColor.RED + "Dctr's Space Helmet");
-                        ArrayList<String> lore = new ArrayList<>();
-                        lore.add(ChatColor.GRAY + "" + ChatColor.ITALIC + "Drop this item to decrease your FPS");
-                        lore.add(ChatColor.GRAY + "" + ChatColor.ITALIC + "Immune to drops");
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                itemMeta.setDisplayName(ChatColor.RED + "Dctr's Space Helmet");
+
+                itemMeta.setLore(Arrays.asList(
+                        lore.add(ChatColor.GRAY + "" + ChatColor.ITALIC + "A rare space helmet forged");
+                        lore.add(ChatColor.GRAY + "" + ChatColor.ITALIC + "from shards of moon glass.");
                         lore.add("");
                         lore.add(ChatColor.DARK_GRAY + "To: " + player.getDisplayName());
                         lore.add(ChatColor.DARK_GRAY + "From: " + ChatColor.RED + "[ADMIN] " + ((Player)sender).getDisplayName());
@@ -47,11 +49,16 @@ public class SpaceHelmetCmd implements CommandExecutor {
                         lore.add("");
                         lore.add(ChatColor.DARK_GRAY + "This item can be reforged!");
                         lore.add(ChatColor.RED + "" + ChatColor.BOLD + "SPECIAL HELMET");
-                        itemMeta.setLore(lore);
-                        itemStack.setItemMeta(itemMeta);
-                        player.getInventory().setHelmet(new ItemStack(itemStack));
-                        player.sendMessage("§eYou have been given a§e §cDctr's Space Helmet!§c §e(2022)§e");
-                }
-                return true;
+                ));
+
+                itemStack.setItemMeta(itemMeta);
+                player.getInventory().setHelmet(new ItemStack(itemStack));
+                player.sendMessage("§eYou have been given a§e §cDctr's Space Helmet!§c §e(2022)§e");
+            } else {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cYou don't have access to this command!"));
+            }
         }
+        return true;
+    }
 }
+
